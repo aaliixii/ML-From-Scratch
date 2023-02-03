24bfac712be7ee4254600d82e2036c7c93d9471e
Josh Machado <joshmachado@Joshs-MacBook-Air.local>
2023-02-03 16:46:34 +0530

    Working KNN

--- Nearest Neighbours/knn.py
+++ Nearest Neighbours/knn.py
@@ -6,24 +6,10 @@ class Node():
     def __init__(self, state, location) -> None:
         self.state = state
         self.location = location
-        self.visited = False
 
     def dist(self, node):
         return round(math.dist(self.location, node.location), 2)
 
-class NHood():
-    def __init__(self) -> None:
-        self.neighbours = []
-
-    def addNode(self, node):
-        self.neighbours.append(node)
-
-    def rmNode(self, node):
-        self.neighbours.remove(node)
-
-    def maxDist(self, node):
-        dist = [math.dist(i.location, node.location) for i in self.neighbours]
-        return (max(dist), self.neighbours[dist.index(max(dist))])
 
 def plot(nodes, neighbours, target):
     x = []
@@ -34,7 +20,7 @@ def plot(nodes, neighbours, target):
     for node in nodes:
         x.append(node.location[0])
         y.append(node.location[1])
-        if node in neighbours:
+        if node.state in neighbours:
             c.append(color[0])
         else:
             c.append(color[1])
@@ -48,7 +34,7 @@ def plot(nodes, neighbours, target):
 
 def main():
     n = int(input('Number of Points'))
-
+    k = int(input('No of Nearest Neighbours'))
     x = [chr(ord('a') + i) for i in range(n)]
     d = [(np.random.randint(-10, 10), np.random.randint(-10, 10)) for i in range(n)]
 
@@ -56,24 +42,13 @@ def main():
     for i in range(len(x)):
         nodes.append(Node(x[i], d[i]))
 
-    print(d)
-    hood = NHood()
     target = Node('target', (0,0))
+    dist = [[i.state, i.dist(target)] for i in nodes]
+    dist.sort(key = lambda i: i[1])
+    neighbours = [dist[i][0] for i in range(k)]
 
-    for i, node in enumerate(nodes):
-        if i<round(np.sqrt(n)):
-            hood.addNode(node)
-        else:
-            if hood.maxDist(node)[0] > node.dist(target):
-                hood.rmNode(hood.maxDist(node)[1])
-                hood.addNode(node)
+    plot(nodes, neighbours, target)
 
-    for i in nodes:
-        print(i.state, i.dist(target))
-
-    for i in hood.neighbours:
-        print(i.state)
-
-    plot(nodes, hood.neighbours, target)
 if __name__ == '__main__':
     main()
+
