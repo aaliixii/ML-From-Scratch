93edacffe43b2ec885b99e474c62d7fb455ec803
Josh <52972578+aaliixii@users.noreply.github.com>
2023-02-04 09:46:40 +0530

    Updated Fib_Search

--- Optimization Algorithms/Bracketing/fib_search.py
+++ Optimization Algorithms/Bracketing/fib_search.py
@@ -1,46 +1,18 @@
-def fib(x, memo):
-    if memo[x] is not None:
-        return memo[x]
-
-    if x<=2:
+def fib(n, memo):
+    if memo[n] is not None:
+        return memo[n]
+    
+    if n == 1 or n == 2:
         return 1
     else:
-        result = fib(x-1, memo) + fib(x-2, memo)
-    memo[x] = result
-    return result
-
-def fib_search(arr, n, x):
-    k = 0
-    fibM = 0
-    memo = [None] * (n + 1)
-
-    while True:
-        if fib(k, memo) >= n:
-            fibM = fib(k, memo)
-            break
-        k+=1
-    fibMm1 = fib(k-1, memo)
-    fibMm2 = fib(k-2, memo)
-
-
-
-
+        result = fib(n - 1, memo) + fib(n - 2, memo)
+        memo[n] = result
+        return result
 
 def main():
-    x = int(input('Target:\n'))
-    arr = []
-    while True:
-        try:
-            arr.append(int(input('Enter number into Array.\tType q to quit\n')))
-        except ValueError:
-            break
-    fib_search(arr, len(arr), x)
-    return
-    found, idx = fib_search(arr, len(arr), x)
-    if found:
-        print(f'Target Found in Array at {idx}')
-    else:
-        print('Target Not Found')
+    n = int(input())
+    memo = [None] * (n + 1)
+    print(fib(n, memo))
 
 if __name__ == '__main__':
     main()
